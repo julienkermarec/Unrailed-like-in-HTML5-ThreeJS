1ee0ab975c7986eda16ef241ea33b9cca054dbab
KERMAREC Julien <julien.kermarec@gmail.com>
2019-10-28 17:43:26 +0100

    multiplayer

--- multiplayer.js
+++ multiplayer.js
@@ -3,40 +3,62 @@ var chatOutput = document.getElementById('chat-output');
 var chatInput = document.getElementById('chat-input');
 var startButton = document.getElementById('setup-datachannel');
 var startSoloButton = document.getElementById('start-solo');
+var menu_principal = document.getElementById('menu_principal');
+var menu_multi = document.getElementById('menu_multi');
+var cancelDatachannel = document.getElementById('cancel-datachannel');
 var menu = document.getElementById('menu');
+let channel;
 chatInput.onkeypress = function (e) {
     if (e.keyCode != 13) return;
     channel.send(this.value);
-    chatOutput.innerHTML = 'Me: ' + this.value + '<hr />' + chatOutput.innerHTML;
+    chatOutput.innerHTML += '<li>Moi: ' + this.value + '</li>';
     this.value = '';
 };
-
-var channel = new DataChannel();
-
-channel.onopen = function (userid) {
-    chatInput.style.display = 'block';
-    startButton.style.display = 'none';
-};
-
-channel.onmessage = function (message, userid) {
-    chatOutput.innerHTML = userid + ': ' + message + '<hr />' + chatOutput.innerHTML;
-};
-
-channel.onleave = function (userid) {
-    chatOutput.innerHTML = userid + ' Left.<hr />' + chatOutput.innerHTML;
-
-    startButton.style.display = 'block';
-    chatInput.style.display = 'none';
-};
-
-// search for existing data channels
-channel.connect();
-
 startSoloButton.onclick = function () {
     menu.style.display = 'none';
     start();
 }
+cancelDatachannel.onclick = function () {
+    console.log("cancel", channel);
+    menu_principal.style.display = 'block';
+    menu_multi.style.display = 'none';
+    delete channel;
+}
+
 startButton.onclick = function () {
     // setup new data channel
-    channel.open();
+    console.log("startButton");
+    menu_principal.style.display = 'none';
+    menu_multi.style.display = 'block';
+
+
+    channel = new DataChannel('unrailed');
+
+    channel.onopen = function (userid) {
+        console.log("on open", userid);
+        chatInput.style.display = 'block';
+        startButton.style.display = 'none';
+        chatOutput.innerHTML += '<li>Connecté avec ' + userid + '</li>';
+
+        menu.style.display = 'none';
+        start();
+    };
+
+    channel.onmessage = function (message, userid) {
+        chatOutput.innerHTML += '<li>' + userid + ': ' + message + '</li>';
+    };
+
+    channel.onleave = function (userid) {
+        chatOutput.innerHTML += '<li>' + userid + ' Left.</li>';
+
+        chatInput.style.display = 'none';
+        menu_principal.style.display = 'block';
+        menu_multi.style.display = 'none';
+    };
+
+    // search for existing data channels
+
+    // channel.open();
+    // channel.connect();
+
 };
